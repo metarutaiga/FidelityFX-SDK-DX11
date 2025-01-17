c0d6dc938a744dab9ac50e7935bec9a0053f06af
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2025-01-17 20:31:51 +0800

    Update

--- FidelityFX/host/backends/blob_accessors/permutations/ffx_fsr3upscaler_pass_permutations.bat
+++ FidelityFX/host/backends/blob_accessors/permutations/ffx_fsr3upscaler_pass_permutations.bat
@@ -1,17 +1,18 @@
 @echo off
 rem This file is part of the FidelityFX SDK.
-rem 
-rem Copyright (c) 2023 Advanced Micro Devices, Inc. All rights reserved.
+rem
+rem Copyright (C) 2024 Advanced Micro Devices, Inc.
 rem 
 rem Permission is hereby granted, free of charge, to any person obtaining a copy
-rem of this software and associated documentation files (the "Software"), to deal
+rem of this software and associated documentation files(the "Software"), to deal
 rem in the Software without restriction, including without limitation the rights
-rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 rem copies of the Software, and to permit persons to whom the Software is
-rem furnished to do so, subject to the following conditions:
+rem furnished to do so, subject to the following conditions :
+rem
 rem The above copyright notice and this permission notice shall be included in
 rem all copies or substantial portions of the Software.
-rem 
+rem
 rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
